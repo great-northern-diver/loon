ce3670e6693be2eb0851cc7c6a0d23d676f66c88
Adrian Waddell <adrian@waddell.ch>
2016-08-04 21:17:51 +0200

    add file to document data sets

---
+++ R/R/datasets.R
@@ -0,0 +1,18 @@
+#' Data for to re-create Hans Rosling's fameous animation
+#'
+#' This data contains ...
+#'
+#' \itemize{
+#'  \item Country. TODO
+#'  \item Year. TODO
+#'  \item Population. TODO
+#'  \item LifeExpectancy. TODO
+#'  \item Fertility. TODO
+#'  \item Income. TODO
+#'  \item Geographic.Region. TODO
+#'  \item Geographic.Region.ID. TODO
+#' }
+#'
+#' @format A data frame with 9855 rows and 8 variables
+#' @source \url{https://www.gapminder.org/}
+"UsAndThem"
