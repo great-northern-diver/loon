c0b20c8dd1eb46731c69c29dba2934cdbb562c33
Adrian Waddell <adrian@waddell.ch>
2017-02-05 20:07:03 +0100

    added color related R documentation

---
+++ R/man-roxygen/param_target.R
@@ -0,0 +1,4 @@
+#' @param target either an object of class loon or a vector that specifies the
+#'   widget, layer, glyph, navigator or context completely. The widget is
+#'   specified by the widget path name (e.g. \code{'.l0.plot'}), the remaining
+#'   objects by their ids.
