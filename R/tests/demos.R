c4f735aab4354d5c3c242c2bd75a4242b4c3944a
Adrian Waddell <adrian@waddell.ch>
2017-07-14 14:07:00 -0700

    update the demos test to omit the execution of the l_ng_* demos under windows

--- R/tests/demos.R
+++ R/tests/demos.R
@@ -4,6 +4,14 @@ library(loon)
 
 # options(demo.ask=FALSE)
 
-for(demo in demo(package="loon")$results[,"Item"])
-  if (!(demo %in% c("loon", "lsystem")))
-    demo(demo, package="loon", character.only=TRUE)
+is_windows <- Sys.info()['sysname'] == "Windows"
+
+for(demo in demo(package="loon")$results[,"Item"]) {
+    if (!(demo %in% c("loon", "lsystem"))) {
+        # on windows all the image resizing seem to use up too much memory
+        # when all the demos are run sequentially
+        if (!(is_windows && grepl("^l_ng_", demo))) {
+            demo(demo, package="loon", character.only=TRUE)            
+        }
+    }
+}
