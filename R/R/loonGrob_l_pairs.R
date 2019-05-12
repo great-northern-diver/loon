633e9017d04e8b9a3a4efc8bdcf6e8c29f201772
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2019-05-12 19:45:30 -0400

    Tidied up l_getLocations() of l_pairs and l_plot_ts to return the data structure common to all l_compounds.
    Also l_get_arrangeGrobArgs() for l_pairs
    updated to use l_getLocations.

--- R/R/loonGrob_l_pairs.R
+++ R/R/loonGrob_l_pairs.R
@@ -7,10 +7,12 @@ loonGrob_layoutType.l_pairs <- function(target) {
 
 #' @export
 l_get_arrangeGrobArgs.l_pairs <- function(target) {
+
     nPlots <- length(target)
     nScatterplots <- nHistograms <- nSerialAxes <- 0
     scatterplots <- histograms <- serialAxes <- list()
     plotNames <- names(target)
+
     for(i in 1:nPlots) {
         if("l_plot" %in% class(target[[i]])) {
             nScatterplots <- nScatterplots + 1
@@ -29,32 +31,19 @@ l_get_arrangeGrobArgs.l_pairs <- function(target) {
         }
     }
 
+    locations <- l_getLocations(target)
+    layout_matrix <- locations$layout_matrix
+
     nvar <- (-1 + sqrt(1 + 8 * nScatterplots)) / 2 + 1
     showSerialAxes <- (nSerialAxes > 0)
     showHistograms <- (nHistograms > 0)
+
     if(showHistograms) {
         histLocation <- if(nHistograms == (nvar - 1) * 2) "edge" else "diag"
-        if(histLocation == "edge") {
-            cells <- nvar + 1
-            showTexts <- TRUE
-        } else {
-            cells <- nvar
-            showTexts <- FALSE
-        }
-    } else {
-        cells <- nvar
-        showTexts <- TRUE
-    }
+        showTexts <- if(histLocation == "edge") TRUE else FALSE
+    } else showTexts <- TRUE
 
-    layout_matrix <- matrix(rep(NA, (cells)^2), nrow = cells)
     scatter_hist <- c(scatterplots, histograms)
-
-    for(i in 1:length(scatter_hist)) {
-        nameOfScatter_hist <- names(scatter_hist[i])
-        pos <- xy_layout(nameOfScatter_hist)
-        layout_matrix[pos$y, pos$x] <- i
-    }
-
     scatter_histGrobs <- lapply(1:(nScatterplots + nHistograms),
                                 function(i){
                                     pi <- scatter_hist[[i]]
@@ -68,43 +57,6 @@ l_get_arrangeGrobArgs.l_pairs <- function(target) {
                                 }
     )
 
-    if(showTexts) {
-        texts <- unique(
-            c(sapply(scatterplots,
-                     function(s){
-                         s['ylabel']
-                     }),
-              sapply(scatterplots,
-                     function(s){
-                         s['xlabel']
-                     })
-            )
-        )
-        nTexts <- length(texts)
-        textGrobs <- lapply(texts,
-                            function(t) {
-                                textGrob(t, gp = gpar(fontsize = 9), name = paste("text", i))
-                            }
-        )
-
-        if(cells == nvar) {
-            for(i in 1:nTexts) {
-                layout_matrix[i, i] <- (nScatterplots + nHistograms) + i
-            }
-        } else {
-            for(i in 1:nTexts) {
-                layout_matrix[(i + 1), i] <- (nScatterplots + nHistograms) + i
-            }
-        }
-    } else  nTexts <- 0
-
-    serialAxesSpan <- floor(nvar/2)
-    # square space
-    for(i in 1:serialAxesSpan) {
-        for(j in 1:serialAxesSpan) {
-            layout_matrix[cells - serialAxesSpan + i, j] <- nScatterplots + nHistograms + nTexts + 1
-        }
-    }
     serialAxesGrob <- unname(
         lapply(serialAxes,
                function(s) {
@@ -116,16 +68,54 @@ l_get_arrangeGrobArgs.l_pairs <- function(target) {
         )
     )
 
-    lgrobs <- if(showTexts) {
-        c(scatter_histGrobs, textGrobs, serialAxesGrob)
-    } else {
-        c(scatter_histGrobs, serialAxesGrob)
+    if(showTexts) {
+
+        texts <- unique(
+            c(sapply(scatterplots,
+                     function(s){
+                         s['ylabel']
+                     }),
+              sapply(scatterplots,
+                     function(s){
+                         s['xlabel']
+                     })
+            )
+        )
+
+        textGrobs <- lapply(texts,
+                            function(t) {
+                                textGrob(t, gp = gpar(fontsize = 9), name = paste("text", i))
+                            }
+        )
+
+        max_value <- max(layout_matrix, na.rm = TRUE)
+        text_index <- (max_value + 1): (max_value + length(texts))
+        if(!showHistograms) {
+
+            diag(layout_matrix) <- text_index
+        } else {
+            if(histLocation == "diag") {
+
+                diag(layout_matrix) <- text_index
+            } else {
+
+                lapply(1:length(text_index),
+                       function(i){
+                           layout_matrix[i + 1, i] <<- text_index[i]
+                       }
+                )
+            }
+        }
+
+        locations$layout_matrix <- layout_matrix
     }
 
-    list(
-        grobs = lgrobs,
-        layout_matrix = layout_matrix,
-        name = "pairs"
+    c(
+        locations,
+        list(
+            grobs = c(scatter_histGrobs, serialAxesGrob, if(showTexts) textGrobs else NULL),
+            name = "pairs"
+        )
     )
 }
 
