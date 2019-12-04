1f747f39169b4c5f4a81709535ee4fd1012a0176
Ted <wpw199414@gmail.com>
2019-12-04 00:07:16 -0500

    update for python

---
+++ Python/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="diver", # Replace with your own username
+    version="1.2.5",
+    author="Example Author",
+    author_email="author@example.com",
+    description="A small example package",
+    long_description= "test",
+    url="https://github.com/pw2wang/loon",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires='>=3.6',
+)
