c608e7dff7bc9e43688d62e22c757da024c5d375
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2020-05-08 13:57:45 -0400

    Removed dead URL

--- R/man-roxygen/templ_sp.R
+++ R/man-roxygen/templ_sp.R
@@ -1,26 +1,25 @@
 #' @description Methods to plot map data defined in the \code{\link[sp]{sp}}
 #'   package
-#' 
+#'
 #' @param widget widget widget path as a string or as an object handle
 #' @param x an object defined in the class \code{\link[sp]{sp}}
 #' @param ... arguments forwarded to the relative \code{\link{l_layer}} function
-#' 
+#'
 #' @details Note that currently loon does neither support holes and ring
 #'   directions.
-#' 
+#'
 #' @references Applied Spatial Data Analysis with R by Bivand, Roger S. and
 #'   Pebesma, Edzer and Gomez-Rubio and Virgilio
-#'   \url{http://www.springer.com/us/book/9781461476177}
 #'
-#' 
+#'
 #' @return layer id
-#'  
+#'
 #' @seealso \code{\link[sp]{sp}}, \code{\link{l_layer}}
 #'
 #' @examples
-#' 
+#'
 #' if (requireNamespace("rworldmap", quietly = TRUE)) {
-#'     world <- rworldmap::getMap(resolution = "coarse") 
+#'     world <- rworldmap::getMap(resolution = "coarse")
 #'     p <- l_plot()
 #'     lmap <- l_layer(p, world, asSingleLayer=TRUE)
 #'     l_scaleto_world(p)
