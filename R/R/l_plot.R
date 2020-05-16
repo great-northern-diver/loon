c89606ba106bffe1ad66155e981c5acf102e1cb2
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2020-05-16 11:09:00 -0400

    fixed the documentation on l_plot.default

--- R/R/l_plot.R
+++ R/R/l_plot.R
@@ -116,7 +116,7 @@ l_plot <- function(x, y, ...) {
 #' on which \code{layout} is used.  For each value of \code{layout}, the scales are connected
 #' as follows:
 #' \itemize{
-#' \item{\code{layout = "wrap:}  Across all facets, when \code{connectedScales} is
+#' \item{\code{layout = "wrap":}  Across all facets, when \code{connectedScales} is
 #'    \itemize{
 #'    \item{\code{"x"}, then  only the "x"  scales are connected}
 #'    \item{\code{"y"}, then only the "y" scales are connected}
@@ -125,7 +125,7 @@ l_plot <- function(x, y, ...) {
 #'    For any other value, only the "y" scale is connected.
 #'    }
 #'    }
-#' \item{\code{layout = "grid:}  Across all facets, when \code{connectedScales} is
+#' \item{\code{layout = "grid":}  Across all facets, when \code{connectedScales} is
 #'    \itemize{
 #'    \item{\code{"cross"}, then only the scales in the same row and the same column are connected}
 #'    \item{\code{"row"}, then both "x" and "y" scales of facets in the same row are connected}
