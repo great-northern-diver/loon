d3aea0bd8b3050cdbdfb0810ea73709dd3f9a3c9
Zehao Xu <z267xu@uwaterloo.ca>
2021-05-09 16:40:03 -0400

    It should be able to recognize the `NULL` is not a loon widget

--- R/R/l_isLoonWidget.R
+++ R/R/l_isLoonWidget.R
@@ -1,22 +1,19 @@
 
 #' @title Check if a widget path is a valid loon widget
-#'   
+#'
 #' @description This function can be useful to check whether a loon widget is
 #'   has been closed by the user.
-#'   
+#'
 #' @template param_widget
-#'   
-#' @return boolean, TRUE if the argument is a valid loon widget path, FALSE 
+#'
+#' @return boolean, TRUE if the argument is a valid loon widget path, FALSE
 #'   otherwise
-#'   
+#'
 #' @export
 l_isLoonWidget <- function(widget) {
-    if(is.list(widget)){
-        FALSE
-    }else{
-        isLoon <- as.logical(as.character(tcl('::loon::isKnownWidget', widget)))
-        
-        if (isLoon) TRUE
-        else FALSE
-    }
+
+    if(is.null(widget)) return(FALSE)
+    if(is.list(widget)) return(FALSE)
+
+    as.logical(as.character(tcl('::loon::isKnownWidget', widget)))
 }
