cc1a88e898c9c4e77944d795b2e27ed2ff7f17de
R. Wayne Oldford <rwoldford@uwaterloo.ca>
2022-02-06 19:10:29 -0500

    Updated doc and rebuild instructions

--- Rebuild_instructions.R
+++ Rebuild_instructions.R
@@ -3,6 +3,10 @@
 # make replaces the tcl library inside R
 #
 # build documentation
+devtools::document(roclets = c('rd', 'collate', 'namespace'))
+# run tests
+devtools::test()
 # check as CRAN
+devtools::check(args = c('--as-cran'))
 pkgdown::build_site()
 devtools::check_win_devel()
